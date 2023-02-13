37a0a9015d5912679c287e2106c2299190422356
ThomasSteendijk <52360610+ThomasSteendijk@users.noreply.github.com>
2023-02-13 12:50:15 +0100

    Fixed some logic in the detection of the winget application

---
+++ Intune/MicrosoftTeams_Evergreen/Configuration/RequirementsScript.ps1
@@ -0,0 +1,9 @@
+if (!(test-path C:\temp)) {mkdir C:\temp | out-null}
+$LF = "C:\temp\log.log"
+$AN = "microsoft.teams"
+"[$(get-date)] [Requirement check] Starting winget search for $AN" | Out-File -Append -FilePath $LF
+$wg = $(winget list $AN --accept-source-agreements) 
+if ($wg -like "*Available*") {
+	"[$(get-date)] [Requirement check] $AN detected but out-of-date"| Out-File -Append -FilePath $LF
+	$true
+} else {"[$(get-date)] [Requirement check] $AN not detected or up-to-date"| Out-File -Append -FilePath $LF}
