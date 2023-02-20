0ecde42ac6c8a9c433225fe5ec164f1dda1833ae
ThomasSteendijk <52360610+ThomasSteendijk@users.noreply.github.com>
2023-02-20 20:26:05 +0100

    Fixed error in detection script for if VC was not installed yet causing it to never be installed

--- Intune/Microsoft VCRedist 2015+ (64-bit)_EverGreen/Configuration/InstalledScript.ps1
+++ Intune/Microsoft VCRedist 2015+ (64-bit)_EverGreen/Configuration/InstalledScript.ps1
@@ -4,7 +4,7 @@ $AN = "Microsoft.VCRedist.2015+.x64"
 "[$(get-date)] [Detection] Starting winget search for $AN" | Out-File -Append -FilePath $LF
 $exe = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe" | Select-Object -ExpandProperty Path
 $wg = $(.$exe list $AN --accept-source-agreements) 
-if (!($wg -like "*Available*") -and !($wg -like "*No installed package*")) {
+if (!($wg -like "*Available*") -and !($wg -like "*No installed package*") -and ($wg)) {
 	"[$(get-date)] [Detection] $AN installed"|Out-File -Append -FilePath $LF
 	$true
 } else {"[$(get-date)] [Detection] $AN not installed"|Out-File -Append -FilePath $LF}
